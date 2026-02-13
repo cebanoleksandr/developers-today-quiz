601933996f51a7573ce72f22dec47def29ca6ebd
Oleksandr Cheban <cebanoleksandr@gmail.com>
2026-02-13 11:11:30 +0200

    Change BaseUrl 2

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'developers-today-quizz-be-ynvo-rfeqzn768.vercel.app',
+  baseURL: 'https://developers-today-quizz-be-ynvo.vercel.app',
   headers: {
     'Content-Type': 'application/json',
   }
